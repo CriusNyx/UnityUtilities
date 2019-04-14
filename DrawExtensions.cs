0529967d3b5d4b81ae8505c1d5bce8b1a82a436a
CriusNyx <RJReynoldsW@gmail.com>
2019-04-14 13:29:26 -0400

    first commit

---
+++ DrawExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DrawExtensions
+{
+    public static void DrawCross(this Vector3 point, float size, float time = -1f)
+    {
+        DrawCross(point, size, Color.white, time);
+    }
+
+    public static void DrawCross(this Vector3 point, float size, Color color, float time = -1f)
+    {
+        Debug.DrawRay(point, Vector3.left * size, color, time);
+        Debug.DrawRay(point, Vector3.right * size, color, time);
+        Debug.DrawRay(point, Vector3.up * size, color, time);
+        Debug.DrawRay(point, Vector3.down * size, color, time);
+        Debug.DrawRay(point, Vector3.forward * size, color, time);
+        Debug.DrawRay(point, Vector3.back * size, color, time);
+    }
+}
