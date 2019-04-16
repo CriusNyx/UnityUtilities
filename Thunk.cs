8be7f20a2e517093513225163ad5ca92bce5ffb1
CriusNyx <RJReynoldsW@gmail.com>
2019-04-16 15:04:54 -0400

    Added Thunk

---
+++ Thunk.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thunk<T>
+{
+    private bool ready = false;
+    private T value;
+    public T Value
+    {
+        get
+        {
+            if (!ready)
+            {
+                value = generate();
+                ready = true;
+            }
+            return value;
+        }
+    }
+
+    private readonly Func<T> generate;
+
+    public Thunk(Func<T> generate)
+    {
+        this.generate = generate;
+    }
+
+    public static implicit operator T (Thunk<T> thunk)
+    {
+        return thunk.Value;
+    }
+}
