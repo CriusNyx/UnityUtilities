7050dc2fb39afca370a97ac206a1541d2dab0b13
CriusNyx <RJReynoldsW@gmail.com>
2019-04-18 16:59:39 -0400

    Added wrapper + reorginization

---
+++ Scripts/DataStructures/Wrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IReadOnlyWrapper<T>
+{
+    T Get();
+}
+
+public interface IWrapper<T> : IReadOnlyWrapper<T>
+{
+    T Value { get; set; }
+}
+
+public class Wrapper<T> : IWrapper<T>
+{
+    public T Value { get; set; }
+
+    public Wrapper()
+    {
+    }
+
+    public Wrapper(T value)
+    {
+        Value = value;
+    }
+    
+    public T Get()
+    {
+        return Value;
+    }
+}
