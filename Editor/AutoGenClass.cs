f1fbd8e29449ad6f22d7d5f7bf99ce434b7d2b00
CriusNyx <RJReynoldsW@gmail.com>
2019-05-04 22:55:12 -0400

    Fixed bug with gitignore

---
+++ Editor/AutoGenClass.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityUtilities
+{
+    public static class AutoGenClass
+    {
+        public static void Generate(string path, string classCode)
+        {
+            string outFileDir = Path.GetDirectoryName(path);
+
+            if (!File.Exists(path))
+            {
+                Directory.CreateDirectory(outFileDir);
+                File.WriteAllText(path, classCode);
+                AssetDatabase.Refresh();
+            }
+            else
+            {
+                string outFileText = File.ReadAllText(path);
+                if (outFileText != classCode)
+                {
+                    Directory.CreateDirectory(outFileDir);
+                    File.WriteAllText(path, classCode);
+                    AssetDatabase.Refresh();
+                }
+            }
+        }
+    }
+}
